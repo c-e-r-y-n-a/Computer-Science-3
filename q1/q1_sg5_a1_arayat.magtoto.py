6e26f94235c95a975e4b27076b6d9b97387822da
c-e-r-y-n-a <cpmagtoto2030@clc.pshs.edu.ph>
2026-08-27 09:21:07 +0800

    Create Hero class with damage handling
    Define a Hero class with methods for taking damage and user input for name and HP.

---
+++ q1/q1_sg5_a1_arayat.magtoto.py
@@ -0,0 +1,16 @@
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+    def take_damage(self, hp):
+        self.hp = self.hp - amount
+        print(f"You took {amount} damage")
+        print("Arthur took 10 damage")
+        print("Morgana is at full HP")
+
+name = input("Enter your name ")
+hp = input("Enter your HP ")
+amount = int(input("How much damage do you take? "))
+
+
+
